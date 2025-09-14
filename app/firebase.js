1156c36c01b8fbaccc1f3e27851306a069ac2395
Samir Singh <samir@antino.com>
2025-09-14 14:36:25 +0530

    firebase setup done

---
+++ app/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTcBuhO70j0YOLGvPvFkVf4w_ke50DW44",
+  authDomain: "fir-tutorial-ac90b.firebaseapp.com",
+  projectId: "fir-tutorial-ac90b",
+  storageBucket: "fir-tutorial-ac90b.firebasestorage.app",
+  messagingSenderId: "406424187870",
+  appId: "1:406424187870:web:e3d754af039add2ec6e34f",
+  databaseURL: "https://fir-tutorial-ac90b-default-rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
